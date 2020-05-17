b88f98dde285067c07d7aa1233018d8cd89fcaba
Matteo Pasotti <matteopasotti1@gmail.com>
2020-05-17 20:14:58 +0100

    created MovieCell

---
+++ ios-movies-api-storyboard/ios-movies-api-storyboard/View/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  ios-movies-api-storyboard
+//
+//  Created by Matteo Pasotti on 17/05/2020.
+//  Copyright © 2020 Matteo Pasotti. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
