a31d3d1d0abeddc13b0ee0d5ec2b9fd7f2e2400b
Matteo Pasotti <matteopasotti1@gmail.com>
2020-05-17 20:01:35 +0100

    refactoring packages for MVC

---
+++ ios-movies-api-storyboard/ios-movies-api-storyboard/Controller/MoviesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListViewController.swift
+//  ios-movies-api-storyboard
+//
+//  Created by Matteo Pasotti on 17/05/2020.
+//  Copyright © 2020 Matteo Pasotti. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
